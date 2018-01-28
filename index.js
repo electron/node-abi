2b80e96171b94e02d497a2f8c841aee70342fbf8
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-01-28 13:46:09 +0100

    move node-webkit to additionalTargets + export additionalTargets

--- index.js
+++ index.js
@@ -62,7 +62,10 @@ var supportedTargets = [
   {runtime: 'electron', target: '1.5.0', abi: '51', lts: false},
   {runtime: 'electron', target: '1.6.0', abi: '53', lts: false},
   {runtime: 'electron', target: '1.7.0', abi: '54', lts: false},
-  {runtime: 'electron', target: '1.8.0', abi: '57', lts: false},
+  {runtime: 'electron', target: '1.8.0', abi: '57', lts: false}
+]
+
+var additionalTargets = [
   {runtime: 'node-webkit', target: '0.13.0', abi: '47', lts: false},
   {runtime: 'node-webkit', target: '0.15.0', abi: '48', lts: false},
   {runtime: 'node-webkit', target: '0.18.3', abi: '51', lts: false},
@@ -89,12 +92,16 @@ var deprecatedTargets = [
 
 var futureTargets = []
 
-var allTargets = deprecatedTargets.concat(supportedTargets).concat(futureTargets)
+var allTargets = deprecatedTargets
+      .concat(supportedTargets)
+      .concat(additionalTargets)
+      .concat(futureTargets)
 
 exports.getAbi = getAbi
 exports.getTarget = getTarget
 exports.deprecatedTargets = deprecatedTargets
 exports.supportedTargets = supportedTargets
+exports.additionalTargets = additionalTargets
 exports.futureTargets = futureTargets
 exports.allTargets = allTargets
 exports._getNextTarget = getNextTarget
