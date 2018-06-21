153811ab83ed52bfa929cd49adfcdc22f029790c
Samuel Attard <samuel.r.attard@gmail.com>
2018-06-22 02:13:54 +1000

    fix: electron is in beta, move to future targets
    also add beta tag

--- index.js
+++ index.js
@@ -64,8 +64,7 @@ var supportedTargets = [
   {runtime: 'electron', target: '1.6.0', abi: '53', lts: false},
   {runtime: 'electron', target: '1.7.0', abi: '54', lts: false},
   {runtime: 'electron', target: '1.8.0', abi: '57', lts: false},
-  {runtime: 'electron', target: '2.0.0', abi: '57', lts: false},
-  {runtime: 'electron', target: '3.0.0', abi: '64', lts: false}
+  {runtime: 'electron', target: '2.0.0', abi: '57', lts: false}
 ]
 
 var additionalTargets = [
@@ -94,6 +93,7 @@ var deprecatedTargets = [
 ]
 
 var futureTargets = [
+  {runtime: 'electron', target: '3.0.0-beta.1', abi: '64', lts: false}
 ]
 
 var allTargets = deprecatedTargets
